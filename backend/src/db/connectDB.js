2c9623804ce3a36865f54eff96e79dc59988759e
Venkatesh Yadav <chennaboinavenkateshyadav@gmail.com>
2025-09-24 21:28:32 +0530

    database bug fixed

--- backend/src/db/connectDB.js
+++ backend/src/db/connectDB.js
@@ -4,8 +4,8 @@ const connectDB = async () => {
   try {
     await mongoose.connect(process.env.DATABASE_CONNECTION_URL);
     console.log("Connected to MongoDB");
-
   } catch (error) {
+    console.error("Database connection failed:", error.message);
   }
 };
 
